ceaac5513ecc3c316b368c29bdfdf384c4a8f431
Tim Yun <tim@Tim.local>
2025-12-01 16:26:32 +0900

    fix: suppress hydration warning in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body className={inter.variable} suppressHydrationWarning={true}>
         <AuthProvider>
           <PurchaseProvider>
             <div className="container">
